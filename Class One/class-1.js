a1b6f32271054bfe46eb9fc428fa45e57986fabd
towhidulislam99 <towhidulislam618@gmail.com>
2024-08-27 00:09:21 +0600

    JavaScript Learn Code For Begainer

---
+++ Class One/class-1.js
@@ -0,0 +1,18 @@
+const truth = "Allah always Help me";
+console.log(truth);
+
+//data types in javascripts
+
+let institute = "Bagdha Technical Training Center";
+console.log(institute);
+
+let value = 10;
+console.log(value);
+
+let boolean = false;
+console.log(boolean);
+
+//undefiend Data Types
+
+let user = null;
+console.log(user);
---
+++ Class One/operator.js
@@ -0,0 +1,24 @@
+//operator in js
+let sum = 10 + 5;
+console.log(sum)
+let mul = 10 * 5;
+
+let mango = 20;
+let juice = 40;
+console.log(mango > juice);
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+console.log(true && true);
+console.log(true && false);
+
+console.log(true || false);
+
+console.log(!true)
+console.log(!!true)
+
+const firstName = "Md Towhidul"
+const lastName = "Islam"
+const fullName = firstName + " " + lastName
+console.log(fullName.toUpperCase())
