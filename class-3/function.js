24efbb6574d946013cc9434850323c8502c94e7a
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-01 20:30:21 +0600

    Some Function Work Example

--- class-3/function.js
+++ class-3/function.js
@@ -32,3 +32,32 @@ const celcius = 30;
 const farhenhite = farhenhiteCalculate(celcius);
 
 console.log(farhenhite);
+
+
+function addition(a, b){
+    sum = a + b;
+    return sum;
+}
+
+addition(200, 20);
+console.log (sum)
+
+const multiplication = (x, y) => {
+    const mul = x * y;
+    return mul;
+};
+
+const result6 = multiplication(200, 10);
+console.log(result6);
+
+
+let factorial = (num) => {
+    let sum = 1;
+    for (let i = 1; i <= num; i++) {
+        sum *= i;
+    }
+    return sum;
+};
+
+const valuePai = factorial(10);
+console.log(valuePai);
