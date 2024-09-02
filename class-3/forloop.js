1983332a06807b9b80bf1d74e69a009c27d4c4f4
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-02 22:21:05 +0600

    for Loop Practice

---
+++ class-3/forloop.js
@@ -0,0 +1,22 @@
+for(let i = 0; i < 3; i++){
+    console.log("Hellow");
+    
+}
+
+let i = 0;
+for(; i < 5; ){
+    console.log(i++);
+    
+}
+
+for(let i = 0; i < 10; i++){
+    if(i % 2 == 0){
+        console.log("even");
+        
+    }
+    else{
+        console.log("odd");
+        
+    }
+}
+
