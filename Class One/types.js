6935b67163ef8bbe353d4481e2a309bb93ae233d
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-04 12:35:06 +0600

    Some Practive Variable and types

---
+++ Class One/types.js
@@ -0,0 +1,30 @@
+let name = "Bangladesh"
+console.log(name);
+
+name = "Bagdha"
+console.log(name);
+
+console.log(`Hello ${name}`);
+console.log(`Hello ${"name"}`);
+console.log(`hello ${1}`);
+
+// console.log(typesof(name));
+
+console.log(1/0);
+
+console.log("not a number" / 2);
+
+console.log(NaN + 1);
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+const check = typeof("name")
+console.log(check);
+
+const name1 = null;
+console.log(name1);
+
+
+
+
+
---
+++ Class One/variable.js
@@ -0,0 +1,38 @@
+let message;
+message = "Hello Adrina"
+console.log(message);
+
+let user = "John", age = 19, message1 = `Hello ${user}`;
+
+console.log(user, age, message1);
+
+let mess = "Hello Dom crush";
+
+console.log(mess);
+
+let admin = "John";
+
+name2 = admin;
+
+admin = name2;
+
+console.log(admin);
+
+
+let ourPlannetName = "Earth";
+let currentUserNme = "John";
+
+
+const birthday = '27.10.1997';
+const age1 = someCode(birthday);
+
+console.log(age1);
+
+
+
+
+
+
+
+
+
