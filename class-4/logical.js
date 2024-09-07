56a3820bba51091aceb3aeac6edc31b4344dba8f
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-07 10:46:01 +0600

    Logical Operator Some Practice code

--- class-4/logical.js
+++ class-4/logical.js
@@ -3,3 +3,29 @@
     console.log(" Hellow Bangladesh 2.0");
     
  }
+
+ let hour = 9;
+ let isWeekend = true;
+ if(hour < 10 || hour > 18 || isWeekend){
+   console.log("The Office is Closed");
+   
+ }
+
+ let gonta = 12;
+ let minute = 30;
+ if(gonta == 12 && minute == 30){
+   console.log("Its Time to 12:30 am");
+   
+ }
+
+ let person = "admin & Superadmin";
+ let age = 19;
+
+ if(person == "admin & Superadmin" && age == 20){
+   console.log("You are Passed This Step Please wait for Next Step");
+   
+ }
+ else{
+   console.log("You are not Eligible for this requirement you Fullfill this requirement and Try Again");
+   
+ }
