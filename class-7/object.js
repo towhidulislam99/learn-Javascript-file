b50bc134ccf0b8ac90164e18d664d07310ca27f5
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-11 11:01:11 +0600

    Object practice code here

---
+++ class-7/object.js
@@ -0,0 +1,27 @@
+let person = {
+    name : "Md Towhidul Islam",
+    age : 27,
+}
+
+console.log(`Name is: ${person.name} and the Age is: ${person.age}`);
+
+console.log(person.name = true);
+
+delete person.age;
+
+console.log(person.age);
+
+person.age = 25;
+ console.log(person.age);
+
+ let user = {
+    name : "John Mahmud",
+    age : 30,
+ }
+
+ let key = "name";
+ console.log(user.key);
+ 
+ 
+
+
